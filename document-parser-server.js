8119f795f52bbf7ddd4e82d94d774cf1ad8d2b3f
sonawane-yogesh <sonawane-yogesh@users.noreply.github.com>
2019-10-03 14:44:35 +0530

    initial commit

---
+++ document-parser-server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var expressPath = require('express-path');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var appRoutes = require('./routes/app-routes');
+app.use(bodyParser.json({
+    limit: '60mb'
+}));
+app.use(bodyParser.urlencoded({
+    limit: '60mb',
+    extended: true
+}));
+app.use(cors());
+expressPath(app, appRoutes);
+var port = process.env.PORT || 3004;
+
+app.listen(port, () => {
+    console.log("===============================================");
+    console.log(`Document Parser Server running on port ${port}`);
+    console.log("================================================");
+})
